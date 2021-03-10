5eac3510a36653bc21dcedcdb9e9d8aaace5b41a
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-10 17:25:20 +0000

    Changed return on Null check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l, r;
 
-	if (!tree || (!(tree->left) && !(tree->right)))
+	if (!tree)
 		return (0);
 	l = binary_tree_height(tree->left) + 1;
 	r = binary_tree_height(tree->right) + 1;
@@ -34,7 +34,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if ((tree->left == NULL) && (tree->right == NULL))
-		return (0);
+		return (1);
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
 	if (binary_tree_is_perfect(tree->right) == binary_tree_is_perfect(tree->left))
