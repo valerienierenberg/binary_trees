fee0fc24e855c8326db94549a04f2454a0991b14
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-10 17:23:33 +0000

    Added left and right checks for height func

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l, r;
 
-	if (!tree)
+	if (!tree || (!(tree->left) && !(tree->right)))
 		return (0);
 	l = binary_tree_height(tree->left) + 1;
 	r = binary_tree_height(tree->right) + 1;
