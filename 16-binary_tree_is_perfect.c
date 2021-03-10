543706bda6daf5f721ce953e73c6fac478380b5f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-10 16:26:51 +0000

    Add null check back into height function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,5 +37,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 		return (1);
-	return (0);
+	else
+		return (0);
 }
