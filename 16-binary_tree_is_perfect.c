93f22b57f7e813605f5000729e4fb9c76f873022
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-10 16:20:07 +0000

    remove l and r variables

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,11 +37,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (!(tree->left) && !(tree->right))
 		return (1);
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
-	if (l == r)
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 		return (1);
-	if (l != r)
-		return (0);
 	return (0);
 }
